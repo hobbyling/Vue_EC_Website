1da7d43fbfd042a2a705c1263d71103b606dd63e
hobby <g99949@hotmail.com>
2019-06-15 00:09:11 +0800

    [Add] Dashboard 版型; 登入跳轉頁面修改

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Sidebar/>
+        <div class="container-fluid">
+            <div class="row">
+                <Navbar/>
+
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                    <router-view></router-view>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from './Sidebar';
+import Navbar from './Navbar'
+
+export default {
+    components:{
+        Sidebar,
+        Navbar
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dashboard from '@/components/Dashboard'
+
 import Login from '@/components/pages/Login'
+import Products from '@/components/pages/Products'
 
 Vue.use(Router)
 
@@ -17,6 +20,19 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: 'products',
+          name: 'Products',
+          component: Products,
+          meta: { requiresAuth: true }
+        },
+      ]
     },
     {
       path: '*',
