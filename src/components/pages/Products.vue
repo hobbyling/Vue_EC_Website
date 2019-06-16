b8bcb3c31d33a852c41b31e486b5f041ff660ec1
hobby <g99949@hotmail.com>
2019-06-16 22:51:58 +0800

    [Add] 增加modal樣板

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="text-right mt-4">
-            <button class="btn btn-primary">建立新的產品</button>
+            <button class="btn btn-primary" @click="openModal()">建立新的產品</button>
         </div>
     
 
@@ -30,10 +30,33 @@
                 </tr>
             </tbody>
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="productModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
     data(){
         return{
@@ -47,6 +70,9 @@ export default {
             this.axios.get(api).then((response) => {
                 vm.products = response.data.products
             })
+        },
+        openModal(){
+            $('#productModal').modal('show');
         }
     },
     created(){
