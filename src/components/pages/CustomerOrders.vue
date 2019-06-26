a894d5ef5b8d2d881739e90a59a57299e2ecc1e4
hobby <g99949@hotmail.com>
2019-06-26 23:41:25 +0800

    [Add] 加入購物車功能

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -22,11 +22,57 @@
                             <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
                             查看更多
                         </button>
-                        <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
+                        <button type="button" class="btn btn-outline-danger btn-sm ml-auto" @click="addtoCart(item.id)">
                             <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
                             加到購物車
                         </button>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="cartList container" v-if="carts">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">品名</th>
+                        <th scope="col">數量</th>
+                        <th scope="col">單價</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, key) in carts" :key="key">
+                        <td>
+                            <button class="btn btn-outline-danger">
+                                <i class="fas fa-trash-alt"></i>
+                            </button>
+                        </td>
+                        <td>
+                            <span>{{item.product.title}}</span></br>
+                            <span v-if="item.coupon" class="text-success">已套用優惠券</span>
+                        </td>
+                        <td>{{item.product.num}} / {{item.product.unit}}</td>
+                        <td>{{item.product.price}}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td class="text-right">總計</td>
+                        <td>{{total}}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td class="text-success text-right">折扣價</td>
+                        <td class="text-success">{{finalTotal}}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="請輸入優惠碼"  aria-describedby="button-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">套用優惠碼</button>
                 </div>
             </div>
         </div>
@@ -57,16 +103,16 @@
                             <div class="h4" v-if="product.price">現在只要 {{product.price}} 元</div>
                         </div>
 
-                        <select name="" id="" class="form-control mt-3">
-                            <option value="1">選購 1 件</option>
+                        <select name="" id="" class="form-control mt-3" v-model="product.num">
+                            <option :value="num" v-for="num in 10" :key="num">選購 {{num}} {{product.unit}}</option>
                         </select>
                     </div>
                     <div class="modal-footer">
                         <div class="text-muted text-nowrap mr-3">
                             小計 <strong> {{ product.num * product.price }} </strong> 元
                         </div>
-                        <button type="button" class="btn btn-primary">
-                            <!-- <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i> -->
+                        <button type="button" class="btn btn-primary" @click="addtoCart(product.id, product.num)">
+                            <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i>
                             加到購物車
                         </button>
                     </div>
@@ -84,6 +130,9 @@ export default {
         return{
             products: [],
             product: {},
+            carts: [],
+            total: '',
+            finalTotal: '',
             isLoading: false,
             status:{
                 loadingItem: '',
@@ -92,6 +141,7 @@ export default {
     },
     created(){
         this.getProducts();
+        this.getCart();
     },
     methods: {
         getProducts(){
@@ -109,10 +159,38 @@ export default {
             const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
             vm.status.loadingItem = id;
             this.$http.get(api).then((response) => {
-                console.log(response);
+                // console.log(response);
                 $('#productModal').modal('show');
                 vm.product = response.data.product;
                  vm.status.loadingItem = '';
+                
+            })
+        },
+        addtoCart(id, qty = 1){
+            const vm = this;
+            const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+            const cart = {
+                product_id: id,
+                qty
+            }
+            vm.status.loadingItem = id;
+            this.$http.post(api, {data: cart}).then((response) => {
+                console.log(response);
+                vm.status.loadingItem = '';
+                vm.getCart();
+                $('#productModal').modal('hide');
+            })
+        },
+        getCart(){
+            const vm = this;
+            const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
+            vm.isLoading = true;
+            this.$http.get(api).then((response) => {
+                console.log(response);
+                vm.carts = response.data.data.carts;
+                vm.total = response.data.data.total;
+                vm.finalTotal = response.data.data.final_total;
+                vm.isLoading = false;
                 
             })
         }
