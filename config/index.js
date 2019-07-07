579be9c6abf925c4ab40a6f59586b4792fd5d3d7
hobby <g99949@hotmail.com>
2019-07-07 14:57:02 +0800

    [Revise] config/index.jsk27

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Vue_EC_Website/',
+    assetsPublicPath: '/Vue_EC/',
 
     /**
      * Source Maps
